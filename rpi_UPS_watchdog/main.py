b274e09ae004b7e65e476d7f64f25218849d6c3c
Jesper Wallentin <132165471+Peppson@users.noreply.github.com>
2024-04-05 22:36:06 +0200

    Update main.py

--- rpi_UPS_watchdog/main.py
+++ rpi_UPS_watchdog/main.py
@@ -2,6 +2,7 @@
 from machine import Pin
 start_button = Pin(18, Pin.IN, Pin.PULL_UP) 
 
+# Don't start program if "debug button" is pressed during boot 
 if start_button:
-    import Pico_watchdog 
+    import pico_watchdog 
 
